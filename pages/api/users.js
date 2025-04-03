c0107b289efc64df1d878c219ba576964cd2fd4c
Kaikov1502 <kaikov@gmail.com>
2025-04-03 13:34:50 +0300

    Create users.js

---
+++ pages/api/users.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+  const dummyUsers = [
+    {
+      firstName: "יוסי",
+      lastName: "כהן",
+      email: "kaikov@gmail.com",
+      role: "מנהל",
+      permission: "מלאה"
+    },
+    {
+      firstName: "דנה",
+      lastName: "לוי",
+      email: "dana@example.com",
+      role: "תמיכה",
+      permission: "מוגבלת"
+    }
+  ];
+  res.status(200).json(dummyUsers);
+}
